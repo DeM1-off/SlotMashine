4907b8f3be148a4d3ae8e72b53633075983c9266
Dmitro Dohryk <dmitro.dohryk@gmail.com>
2023-05-04 17:47:16 +0300

    Initial Commit

---
+++ SlotMashine/SlotMashineApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlotMashineApp.swift
+//  SlotMashine
+//
+//  Created by Dmitro Dohryk on 04.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct SlotMashineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
